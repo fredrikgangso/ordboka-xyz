e67178c853935ee8c78cedbbead543ccfe3d7dae
fredrikgangso <fredrik@gangso.dev>
2025-10-09 13:17:09 +0200

    fix: update base path for GitHub Pages compatibility

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import Dictionary from "./components/Dictionary.vue";
 const routes = [{ path: "/", component: Dictionary }];
 
 const router = createRouter({
-  history: createWebHistory("/ordboka-xyz/"), // Match your GitHub Pages base
+  history: createWebHistory("/"), // Match your GitHub Pages base
   routes,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/ordboka-xyz/", // Replace with your GitHub repo name
+  base: "/", // Replace with your GitHub repo name
   build: {
     outDir: "dist",
     assetsDir: "assets",
